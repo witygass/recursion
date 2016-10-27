078ff81278ce91693b4b54639c931b631da0add5
witygass <witygass@indiana.edu>
2016-10-27 17:03:11 -0400

    tests passed

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,47 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj == null) return "null";
+
+  switch (obj.constructor) {
+    case String:
+      return '"' + obj + '"';
+      break;
+
+    case Array:
+      if (obj.length >= 1) {
+        var stringifiedJSON = [];
+
+        _.each(obj, function(el){
+          stringifiedJSON.push(stringifyJSON(el));
+        });
+
+        return '[' + stringifiedJSON.join(",") + ']';
+      } else {
+        return '[]';
+      }
+    break;
+
+    case Object:
+      var keys = Object.keys(obj);
+      if (keys.length >= 1) {
+        var stringifiedJSON = '';
+
+        _.each(keys, function(key, index){
+          if (obj[key] === undefined || key.constructor === Function || typeof obj[key] === 'function') {}
+          else {
+            stringifiedJSON += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+            if (!(index === keys.length - 1)) stringifiedJSON += ',';
+          }
+        });
+
+        return '{' + stringifiedJSON + '}';
+      } else {
+        return '{}';
+      }
+    break;
+
+  }
+  
+  return obj.toString();
 };
